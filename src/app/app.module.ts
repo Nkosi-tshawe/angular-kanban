464b68b8c865444b7edc40d540d06570047da073
Nkosi Tshawe <Nkosi.tag@gmail.com>
2022-09-07 23:08:02 +0200

    dashboard and login components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { allIcons, HeroIconModule} from 'ng-heroicon';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HeroIconModule.forRoot({
+      ...allIcons
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
