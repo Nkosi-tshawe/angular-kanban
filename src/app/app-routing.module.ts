20a650ad290278513892180a9b113254596439b2
Nkosi-tshawe <Nkosi.tag@gmail.com>
2022-09-09 13:19:16 +0200

    feat(component): sigin html component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-const routes: Routes = [{path:'',component:LoginComponent,pathMatch:'full'},
-  {path: 'dashboard', component: DashboardComponent},
+const routes: Routes = [
+  { path:'',component:LoginComponent,pathMatch:'full'},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'sign-up', component: SignUpComponent},
+  { path: 'login', component: LoginComponent},
   {path:'*', redirectTo: ''}
 ];
 
